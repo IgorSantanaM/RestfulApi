918ddb26d777296b66bc93e6782ac5ad21c3fc0e
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-02-11 21:35:17 -0400

    Initial commit

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost(Endpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+            await _movieRepository.CreateAsync(movie);
+            return Created($"/{Endpoints.Movies.Create}/{movie.Id}", movie);
+        }
+        [HttpGet(Endpoints.Movies.Get)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetAsync(id);
+            if(movie is null)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpGet(Endpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+
+            var moviesReponse = movies.MapToResponse();
+
+            return Ok(moviesReponse);
+        }
+    }
+}
