b5af3b691e9165216272982157272db644b5f3d0
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-02-24 17:43:17 -0400

    (feat): Added HATEOAS/PAGINATION/SORTING

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Movies.Api.Auth;
 using Movies.Api.Mapping;
 using Movies.Application.Services;
 using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 
 namespace Movies.Api.Controllers
 {
@@ -25,7 +27,7 @@ namespace Movies.Api.Controllers
             return CreatedAtAction(nameof(Get), new { idOrSlug = movie.Id }, movie);
         }
         [HttpGet(Endpoints.Movies.Get)]
-        public async Task<IActionResult> Get([FromRoute] string idOrSlug, CancellationToken token)
+        public async Task<IActionResult> Get([FromRoute] string idOrSlug, [FromServices] LinkGenerator linkGenerator, CancellationToken token)
         {
             var userId = HttpContext.GetUserId();
             var movie = Guid.TryParse(idOrSlug, out var id) ?
@@ -37,7 +39,31 @@ namespace Movies.Api.Controllers
                 return NotFound();
             }
 
-            var response = movie.MapToResponse();
+            MovieResponse response = movie.MapToResponse();
+            
+            var movieObj = new {id = movie.Id};
+
+            response.Links.Add(new Link
+            {
+                Href = linkGenerator.GetPathByAction(HttpContext, nameof(Get), values: new { idOrSlug = movie.Id })!,
+                Rel = "self",
+                Type = "GET"
+            });
+
+            response.Links.Add(new Link
+            {
+                Href = linkGenerator.GetPathByAction(HttpContext, nameof(Update), values: movieObj)!,
+                Rel = "self",
+                Type = "PUT"
+            });
+
+            response.Links.Add(new Link
+            {
+                Href = linkGenerator.GetPathByAction(HttpContext, nameof(Delete), values: movieObj)!,
+                Rel = "self",
+                Type = "DELETE"
+            });
+
             return Ok(response);
         }
         [HttpGet(Endpoints.Movies.GetAll)]
@@ -47,14 +73,14 @@ namespace Movies.Api.Controllers
             var options = request.MapToOptions()
                 .WithUser(userId);
             var movies = await _movieService.GetAllAsync(options, token);
-
-            var moviesReponse = movies.MapToResponse();
+            var moviesCount = await _movieService.GetCountAsync(request.Title, request.Year, token);
+            var moviesReponse = movies.MapToResponse(request.Page, request.PageSize, moviesCount);
 
             return Ok(moviesReponse);
         }
 
         [HttpPut(Endpoints.Movies.Update)]
-        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request, CancellationToken token)
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request, [FromServices] LinkGenerator linkGenerator, CancellationToken token)
         {
             var movie = request.MapToMovie(id);
             var userId = HttpContext.GetUserId();
@@ -66,6 +92,10 @@ namespace Movies.Api.Controllers
             }
 
             var response = movie.MapToResponse();
+            var movieObj = new { id = movie.Id };
+             
+           
+
             return Ok(response);
         }
 
--- Movies.Api/Mapping/ContractMapping.cs
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -31,11 +31,14 @@ namespace Movies.Api.Mapping
             };
         }
 
-        public static MoviesResponse MapToResponse(this IEnumerable<Movie> movies)
+        public static MoviesResponse MapToResponse(this IEnumerable<Movie> movies, int page, int pageSize, int totalCount)
         {
             return new MoviesResponse
             {
-                Items = movies.Select(MapToResponse)
+                Items = movies.Select(MapToResponse),
+                Page = page,
+                PageSize = pageSize,
+                Total = totalCount
             };
         }
 
@@ -65,7 +68,13 @@ namespace Movies.Api.Mapping
             return new GetAllMoviesOptions()
             {
                 Title = request.Title,
-                YearOfRelease = request.Year
+                YearOfRelease = request.Year,
+                SortField = request.SortBy?.Trim('+', '-'),
+                SortOrder = request.SortBy is null ? SortOrder.Unsorted :
+                    request.SortBy.StartsWith('-') ? SortOrder.Descending :
+                    SortOrder.Ascending,
+                Page = request.Page,
+                PageSize = request.PageSize,
             };
         }
 
